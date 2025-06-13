ae49685a97a792930c09571bfaf17e4ed948b156
Qasem Al-Aqrabi <152648064+QasemMagdi2002@users.noreply.github.com>
2025-06-13 22:43:09 +0300

    Update App.java edited

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World! (Back to Normal)";
+    private static final String MESSAGE = "Hello World! (Back to Edited)";
 
     public App() {}
 
