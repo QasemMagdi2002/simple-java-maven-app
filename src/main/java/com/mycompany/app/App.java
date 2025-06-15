e3afa7e58ba79d577e13ab6ad530297a27e6d241
Qasem Al-Aqrabi <152648064+QasemMagdi2002@users.noreply.github.com>
2025-06-15 19:22:07 +0300

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World! (Edited slightly meow)";
+    private static final String MESSAGE = "Hello World! (Edited)";
 
     public App() {}
 
